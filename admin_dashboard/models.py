492ebd769c26e8a8894f4d2e0354c6383c4b1bb6
Kent <jamila.211148@koronadal.sti.edu.ph>
2023-04-19 22:58:04 +0800

    Sidenav update

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# class SidebarParentNode(models.Model):
+#     title = models.CharField(max_length=30)
+
+#     def __str__(self):
+#         return f"{self.title}"
+
+#     class Meta:
+#         verbose_name_plural = "Parent Nodes"
+
+# class SidebarNodes(models.Model):
+#     parent_node = 
+#     title = models.CharField(max_length=30)
+#     urls = 
+
+#     def __str__(self):
+#         return f"{self.parent_node} {self.title}"
+
+#     class Meta:
+#         verbose_name_plural = "Nodes"

bd4cc4c2912f999c53edce85df9a4e3e5d447310
Kent <jamila.211148@koronadal.sti.edu.ph>
2023-04-22 15:30:48 +0800

    Update tasks.py

--- core/tasks.py
+++ core/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 import time
 
+from .semaphore import send_sms
+
 @shared_task
 def print_hello_world():
-    print("Hello world")
+    send_sms('09772381588', 'Sent from Heroku Server')

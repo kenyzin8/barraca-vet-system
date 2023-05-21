dda71453fc7e5eb6f2a857423f625700a9423ba3
Kent <jamila.211148@koronadal.sti.edu.ph>
2023-05-21 19:56:53 +0800

    Update settings.py

--- barraca/settings.py
+++ barraca/settings.py
@@ -35,11 +35,11 @@ SECRET_KEY = 'django-insecure-)l-r0e3802bh)^4v)5rerq#x+8r=zx=t593=u^l4_u(ee)8^wx
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
 
-if os.getenv('DJANGO_DEBUG') == 'True':
-    DEBUG = True
-else:
-    DEBUG = False
-
+# if os.getenv('DJANGO_DEBUG') == 'True':
+#     DEBUG = True
+# else:
+#     DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition

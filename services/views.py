e8963090957e428357f981fe106ad520ace894dd
Kent <jamila.211148@koronadal.sti.edu.ph>
2023-09-26 19:39:02 +0800

    service valida

--- services/views.py
+++ services/views.py
@@ -24,14 +24,16 @@ def service_add(request):
         if form.is_valid():
             try:
                 new_service = form.save(commit=False) 
+
+                stripped_lower_new_service_type = new_service.service_type.strip().lower()
                 
-                # if Service.objects.filter(service_type=new_service.service_type, active=True).exists():
-                #     form.add_error('service_type', 'Error: Service already exists.')
-                #     context = {
-                #         'form': form,
-                #         'service_types': service_types,
-                #     }
-                #     return render(request, 'services_add.html', context)
+                if Service.objects.filter(service_type__iexact=stripped_lower_new_service_type, active=True).exists():
+                    form.add_error('service_type', 'Error: Service already exists.')
+                    context = {
+                        'form': form,
+                        'service_types': service_types,
+                    }
+                    return render(request, 'services_add.html', context)
                 
                 new_service.save()  
                 return redirect('service-list-page')
@@ -60,7 +62,21 @@ def service_update(request, service_id):
 
         if form.is_valid():
             try:
-                form.save()
+                updated_service = form.save(commit=False)
+
+                stripped_lower_updated_service_type = updated_service.service_type.strip().lower()
+                
+                if Service.objects.exclude(id=service.id).filter(service_type__iexact=stripped_lower_updated_service_type, active=True).exists():
+                    print("Service already exists.")
+                    form.add_error('service_type', 'Error: Service already exists.')
+                    context = {
+                        'form': form,
+                        'service_types': service_types,
+                    }
+                    return render(request, 'services_add.html', context)
+                
+                updated_service.save()
+
                 return redirect('service-update-page', service_id=service.id)
             except ValueError as e:
                 form.add_error('service_type', str(e))
@@ -74,7 +90,20 @@ def service_update(request, service_id):
 def service_delete(request, service_id):
     service = get_object_or_404(Service, id=service_id)
     
+    essential_services = [
+        "Check-up",
+        "Deworming",
+        "Vaccination",
+        "Doctor's Fee",
+        "Follow-up Check-up"
+    ]
+    
     if request.method == 'POST':
+        stripped_lower_service_type = service.service_type.strip().lower()
+
+        if stripped_lower_service_type in [s.strip().lower() for s in essential_services]:
+            return JsonResponse({'result': 'error', 'message': 'This service cannot be deleted.'})
+
         service.active = False
         service.save()
         return JsonResponse({'result': 'success'})
